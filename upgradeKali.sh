13324126639d5bbfb8ed0b14890ade12f7b6a346
D4rkN3bul4 <150406464+D4rkN3bul4@users.noreply.github.com>
2023-11-09 20:34:37 -0600

    Added the first file
    This is my first file to be uploaded.

---
+++ upgradeKali.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LOG_FILE="/var/log/update_script.log"
+
+{
+  date
+  echo "Updating package lists..."
+  sudo apt update
+
+  echo "Upgrading packages..."
+  sudo apt full-upgrade -y
+
+  echo "Rebooting..."
+  sudo reboot
+} >> "$LOG_FILE" 2>&1
